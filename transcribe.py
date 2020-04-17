5f9e4d4d50d718c0241be75f3ed41c1aa66ceb1e
Ashwin <mahesh.ashwin1998@gmail.com>
2020-04-16 22:59:05 -0400

    Finished transcribing part of code

--- transcribe.py
+++ transcribe.py
@@ -1,5 +1,6 @@
 import argparse
 import io
+import os
 
 from google.oauth2 import service_account
 from google.cloud import speech
@@ -7,7 +8,7 @@ from google.cloud.speech import enums, types
 
 credentials = service_account.Credentials.from_service_account_file('keys/GoogleKey.json')
 
-def transcribeFile(speechFile):
+def transcribeFile(speechFile, outputFilePath):
   client = speech.SpeechClient(credentials=credentials)
 
   with io.open(speechFile, 'rb') as audioFile:
@@ -23,6 +24,8 @@ def transcribeFile(speechFile):
 
   response = client.recognize(config, audio)
 
+  buffer = []
+
   for result in response.results:
     alternative = result.alternatives[0]
     transcript = alternative.transcript
@@ -31,12 +34,21 @@ def transcribeFile(speechFile):
       word = wordInfo.word
       startTime = wordInfo.start_time
       endTime = wordInfo.end_time
-      print(f'{word} - {startTime.seconds + startTime.nanos * 1e-9}')
+      buffer.append(f'{word} - {startTime.seconds + startTime.nanos * 1e-9}\n')
+      # print(f'{word} - {startTime.seconds + startTime.nanos * 1e-9}')
+  
+  if not os.path.exists('transcribedSongs'):
+      os.mkdir('transcribedSongs')
+
+  outputFile = open(outputFilePath, 'w')
+  outputFile.write("".join(buffer))
+  outputFile.close()
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-  parser.add_argument(dest='audioFilePath', help="Enter the path to the .FLAC file. Maximum length must be 60 seconds and it must be Mono (not stereo).\nUse command: \nffmpeg -ss {start time(seconds)} -t {seconds to record for} -i {inputFile} {outputFile}\n to trim file.")
+  parser.add_argument(dest='audioFilePath', help="Enter the path to the .FLAC file. Maximum length must be 60 seconds and it must be Mono (not stereo).\nUse command: ./trim [args]+ to trim file.")
   args = parser.parse_args()
-
+  outputFilePath= 'transcribedSongs/' + args.audioFilePath[args.audioFilePath.rfind('/') + 1:len(args.audioFilePath)-5] + '.out'
+  
   # transcribeFile('./testFiles/sickoMode_trimmed.flac')
-  transcribeFile(args.audioFilePath)
+  transcribeFile(args.audioFilePath, outputFilePath)
